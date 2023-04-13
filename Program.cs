ee9c355aff9cd84e1d99566c1afb8004322a7499
Mkratkyy <127093276+Mkratkyy@users.noreply.github.com>
2023-04-14 01:00:41 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿DateTime cas = DateTime.Now;
+int CisloMesice = cas.Month;
+string[] mesíce = { "Leden", "Unor", "Březen", "Duben", "Květen", "Červen", "Červenec", "Srpen", "Záři", "Řijen", "Listopad", "Prosinec" };
+Console.WriteLine(cas);
+foreach (string s in mesíce)
+{
+    Console.WriteLine(s);
+}
+int kdy = Convert.ToInt32(Console.ReadLine());
+
+if (kdy == 12)
+{
+    Console.WriteLine($"Tento měsíc je {mesíce[12]}\nMinulý měsíc byl {mesíce[11]}\nNadcházející měsíc je {mesíce[12]}");
+}
+else if (kdy == 12)
+{
+    Console.WriteLine($"Tento měsíc je {mesíce[11]}\nMinulý měsíc byl {mesíce[10]}\nNadcházející měsíc je {mesíce[11]}");
+}
+else
+{
+    Console.WriteLine($"Tento měsíc je {mesíce[kdy - 2]}\nMinulý měsíc byl {mesíce[kdy - 1]}\nNadcházející měsíc je {mesíce[kdy]}");
+}
